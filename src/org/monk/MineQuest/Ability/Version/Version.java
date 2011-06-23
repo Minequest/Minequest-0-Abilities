af1ff95c3701bff9e07fd61c3f1be156a9cc929a
JMonk <jmonk@theminequest.com>
2011-06-23 12:42:05 +0000

    catching up with renaming

--- src/org/monk/MineQuest/Ability/Version/Version.java
+++ src/org/monk/MineQuest/Ability/Version/Version.java
@@ -1,6 +1,6 @@
 package org.monk.MineQuest.Ability.Version;
 
-import org.monk.MineQuest.Ability.MineQuestVersion;
+import org.monksanctum.MineQuest.Ability.MineQuestVersion;
 
 public class Version implements MineQuestVersion {
 	public int getVersion() {
